cd8d51c7c00b7e4733d66ffce29d7dce9ebcb867
Le Xuan Huy <lexuanhuya5@gmail.com>
2025-03-03 08:54:56 +0700

    update confit

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 30
-    SECRET_KEY: str = "your-secret-key"
+    SECRET_KEY: str = os.getenv('SECRET_KEY')
     ALGORITHM: str = "HS256"
     API_V1_STR: str = "/api/v1"
     DATABASE_URL:str = os.getenv('DATABASE_URL')
